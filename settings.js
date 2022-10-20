facfdc0dffbd9ebffec1174c161c58113559176d
kenlyserio <115886568+kenlyserio@users.noreply.github.com>
2022-10-20 11:09:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,7 +29,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
+global.vcardowner = ['+94767755733'] // owner number
 global.ownername = "😎Shown Fernando❯❯" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: Shown" // github or insta name
